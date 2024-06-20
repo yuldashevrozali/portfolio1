d0ea3d6736202b03e7b0c475064259c8af8a3048
Yuldashevrozali <Yuldashevrozali08@gmail.com>
2024-06-20 16:44:29 +0500

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './components/hero/index.jsx'
 import Header from './components/header1/index.jsx'
 import Skills from './components/skills/index.jsx'
 import Projects from './components/projects/index.jsx'
-import Contact from './components/contact/index.jsx'
+// import Contact from './components/contact/index.jsx'
 
 function App() {
 
@@ -13,9 +13,9 @@ function App() {
      <Header />
      <BrowserRouter>
      <Routes>
-     <Route path='/' element = {<Hero />} ></Route>
-     <Route path='/skills' element = {<Skills />} ></Route> 
-     <Route path='/project' element = {<Projects />} ></Route>
+     <Route path='./' element = {<Hero />} ></Route>
+     <Route path='./skills' element = {<Skills />} ></Route> 
+     <Route path='./project' element = {<Projects />} ></Route>
      {/* <Route path='/contact' element = {<Contact />} ></Route> */}
      </Routes>
       
